79ac9bf19c307d89725baac2afe8a892e22f60ff
Gracie McGill <s1938827@ed.ac.uk>
2023-03-08 15:19:11 +0000

    Add files via upload

--- PCA.py
+++ PCA.py
@@ -3,11 +3,13 @@ Script containing functions to apply PCA to data cube containing maps at differe
 """
 
 import numpy as np 
+import scipy 
 
+    
 def mean_centre(data): 
     """" 
     Function to reshape and mean-centre cube of maps so that PCA can be applied. 
-    Takes in data cube of theshape [nx,ny,nz] and outputs mean-centered cube of shape
+    Takes in data cube of the shape [nx,ny,nz] and outputs mean-centered cube of shape
     [nz, npix] where npix=nx*ny
     """
     axes = np.shape(data)
@@ -18,16 +20,17 @@ def mean_centre(data):
     
     return reshaped
 
+
 def fg_est(data,N):
     """
-    Function to produce a PCA foreground estimate for data. Data input in the shape (Nz,Npix) 
+    Function to produce a PCA foreground estimate for data. Data input must be mean-centered and in the shape (Nz,Npix). 
     Returns a foreground estimate of the same shape.
     """
     axes = np.shape(data)
     B = selection_matrix(axes[0],N)     
     A,S = PCA(data,B)
     FG_est = np.matmul(A,S)      #estimate foreground
-    
+     
     return FG_est
                 
                 
@@ -51,11 +54,51 @@ def PCA(data,B):
     Apply PCA to the data
     """
     cov = np.cov(data)                         #find covariance matrix
-    eig_vals, eig_vecs = np.linalg.eig(cov)    #find eigenvalues and vectors of covariance matrix
-    A = np.matmul(eig_vecs, B)          #construct mixing matrix
+    eig_vals, eig_vecs = scipy.linalg.eig(cov)    #find eigenvalues and vectors of covariance matrix
+    A = np.matmul(eig_vecs.real, B)          #construct mixing matrix
     A_trans = A.transpose() 
     S = np.matmul(A_trans,data)         #find eigensources #mixing matrix projected onto data
+   
     return A,S
 
 
+def clean(data,FG_est): 
+    """
+    Clean the data by removing the estimated foreground. Data must be in shape [nx,ny,nz] and foreground shape [nz,npix].
+    """
+    FG_est = FG_est.swapaxes(0,1) #reshape the foreground
+    FG_est = FG_est.reshape(data.shape) 
+
+    cleaned = data - FG_est #remove the foreground
+    return cleaned
+
+
+def shaping(data,xyz = True):
+    """
+    Function to reshape data from [nx,ny,nz] to [nz,nx,ny] or vice versa.
+    """   
+    if xyz == True:
+        #reshape to shape zxy:
+        axes = data.shape #x,y,z
+        data = np.reshape(data,(axes[0]*axes[1],axes[2]))
+        data = data.swapaxes(0,1)
+        data = data.reshape(axes[2],axes[0],axes[1])
+    else:
+        #reshape from zxy to xyz
+        axes = data.shape #z,x,y
+        data = np.reshape(data,(axes[0],axes[1]*axes[2]))
+        data = np.swapaxes(data,0,1)
+        data = np.reshape(data,(axes[1],axes[2],axes[0]))
+   
+    
+    return data
+
+    
+    
+    
+    
+    
+    
+    
+    
     
